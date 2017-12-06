a9e1b21a2bcbf04e0bfa8943c47f37fc261bbc4d
MariaSev <maria240398@mail.ru>
2017-12-06 13:26:14 +0300

    Seminar 06:11:17

--- MyProject1/js/main.js
+++ MyProject1/js/main.js
@@ -47,6 +47,10 @@ function onGetSuccessCB(sensorData)
     //document.write("y: "+ String(sensorData.y)+"\n");
     console.log("z: " + sensorData.z);
     //document.write("z: "+ String(sensorData.z)+"\n");
+    
+    document.getElementById('accel_x').innerHTML = '<p> x:'+ sensorData.x+'</p>';
+    document.getElementById('accel_y').innerHTML = '<p> y:'+ sensorData.y+'</p>';
+    document.getElementById('accel_z').innerHTML = '<p> z:'+ sensorData.z+'</p>';
 }
 
 function onerrorCB(error)
@@ -64,13 +68,16 @@ function onchangedCB(sensorData) {
     console.log('sensor data: ' + sensorData.x);
     console.log('sensor data: ' + sensorData.y);
     console.log('sensor data: ' + sensorData.z);
-    //document.write("data: "+String(sensorData.x));
+    
+    document.getElementById('accel_x').innerHTML = '<p> x:'+ sensorData.x+'</p>';
+    document.getElementById('accel_y').innerHTML = '<p> y:'+ sensorData.y+'</p>';
+    document.getElementById('accel_z').innerHTML = '<p> z:'+ sensorData.z+'</p>';
     window.onload = function(){
     	var a = document.getElementsByTagName('span')[0];
         a.innerHTML = String(sensorData.x);
         a.innerHTML = String(sensorData.y);
         a.innerHTML = String(sensorData.z);
-    }
+    };
     
 }
 //console.log(accelerMaxBatchCount);
@@ -78,3 +85,29 @@ accelerationSensor.setChangeListener(onchangedCB, 10);
 alert("done");
 accelerationSensor.start(onsuccessCB);
 
+function isInternet() {
+    var xhr = new XMLHttpRequest();
+    var file = 'https://www.yandex.ru/';
+
+    xhr.open('HEAD', file , false);
+
+    try {
+
+        xhr.send();
+
+        if (xhr.status >= 200 && xhr.status < 304) {
+            console.log("Internet!");
+        } else {
+            console.log("Internet error");
+        }
+    } catch (e) {
+        console.log("######## No connection ########");
+        console.log(e);
+        console.log(e.name);
+        console.log(e.message);
+        console.log("############################");
+        
+    }
+}
+
+isInternet();
